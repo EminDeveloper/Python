ae0b92f75e53a905e25f7ef988b7c6dcb224c523
Emin <emin.f.mammadov@gmail.com>
2024-06-19 11:39:46 +0400

    Delete Tutorial/Frameworks directory

--- Tutorial/Frameworks/Django/my-site/blogapp/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index),
-    path("index", views.index),
-    path("blogs", views.blogs),
-    path("blog/<int:id>", views.blog_details)
-]
